164201ab551696ef3694a3039160811c0fcfc6a0
Lau Rivero <A01274144@itesm.mx>
2021-08-23 16:46:30 -0500

    Comandos de consola

---
+++ js.js
@@ -0,0 +1,22 @@
+// Uso de la consola
+console.log("Hola");
+console.info("Estamos ne bloque DAW + BD");
+console.warn("Advertencia");
+console.error("Error");
+console.table()
+
+// Variables, constantes y alcance
+
+// Estructura de control
+
+// Alerts, prompts y confirms, escribir en el html
+
+// Funciones
+
+// Funciones flecha
+
+// Arreglos
+
+// Recorrido de Arreglos
+
+// Objetos 
