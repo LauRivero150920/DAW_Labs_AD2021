e2ad675b1cfc39bad9835a215fd44fbe72bf8d32
Lau Rivero <A01274144@itesm.mx>
2021-08-23 16:58:29 -0500

    Variables, constantes y alcance

--- js.js
+++ js.js
@@ -6,8 +6,23 @@ console.error("Error");
 console.table()
 
 // Variables, constantes y alcance
+const precio = 99.99;
+// var -> Ya no se usa :c
+// let tiene alcance solo dentro del ámbito en el que se declara, es decir entre las llaves {}
+// var tiene alcance en toda la función donde fue declarada
+let tacos = 0;
+var pizzas = 0;
 
 // Estructura de control
+if (true){
+    let gorditas = 3;
+    console.log("Verdadero");
+}
+else{
+    console.log("Falso");
+}
+
+// console.log(gorditas); // Esta linea truena el script por que let murió en el if
 
 // Alerts, prompts y confirms, escribir en el html
 
