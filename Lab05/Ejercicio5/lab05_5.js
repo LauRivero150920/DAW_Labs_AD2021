6e06d574dda37952a8829af19678ba73d0ed9033
Lau Rivero <A01274144@itesm.mx>
2021-08-25 17:29:55 -0500

    Lab05: Ejercicio 5

---
+++ Lab05/Ejercicio5/lab05_5.js
@@ -0,0 +1,12 @@
+let number = window.prompt("Ingresa un número de más de un dígito: ");
+
+function Invert(num){
+    let splitString = num.split("");
+    let reverseNum = splitString.reverse();
+    var reverse = reverseNum.join("");
+    let reverse_int = parseInt(reverse);
+    console.log(reverse);
+    alert("El número invertido es " + reverse);
+}
+
+Invert(number);
