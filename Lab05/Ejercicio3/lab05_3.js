7a4b257f16eff717b20b2a097249623320c58dd8
Lau Rivero <A01274144@itesm.mx>
2021-08-24 21:38:46 -0500

    Avance del ejercicio 3

--- Lab05/Ejercicio3/lab05_3.js
+++ Lab05/Ejercicio3/lab05_3.js
@@ -2,21 +2,34 @@
 // Regresa: La cantidad de números negativos en el arreglo, la cantidad de 0's, 
 // y la cantidad de valores mayores a 0 en el arreglo.
 
-let numbers = [];
+let numbers1 = [];
+let numbers2 = [];
 let ceros = 0;
 let negative = 0;
 let positive = 0;
 
-for(let i = 0; i < 100; i++){
-    numbers[i] = window.prompt("Ingresa un número");
-    if(numbers[i] === 0){
-        ceros++;
-    }
-    else if(numbers[i] < 0){
-        negative++;
-    }
-    else{
-        positive++;
-    }
+for(let i = 0; i < 40; i++){
+    numbers1[i] = Math.floor(Math.random() * 100);
 }
+
+function counter(numbers){
+    for(let i = 0; i < length(numbers); i++){
+        if(numbers[i] === 0){
+            ceros++;
+        }
+        else if(numbers[i] < 0){
+            negative++;
+        }
+        else{
+            positive++;
+        }
+    }
+    return ceros, positive, negative;
+}
+
+console.log("Hay ", counter(numbers1), " ceros");
+console.log("Hay ", negative, " numeros negativos");
+console.log("Hay ", positive, " numeros positivos");
+
+
 
