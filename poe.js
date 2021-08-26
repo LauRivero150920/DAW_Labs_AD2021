485847b6a968c31d0bd6c69290bb1333f053fe0b
Lau Rivero <A01274144@itesm.mx>
2021-08-26 16:51:28 -0500

    Eventos en js de manera tradicional

---
+++ poe.js
@@ -0,0 +1,9 @@
+function colorea_azul(){
+    let elemento = document.getElementById("titulo_menu");
+    elemento.style.color = "blue";
+}
+
+function colorea_verde(){
+    let elemento = document.getElementById("titulo_menu");
+    elemento.style.color = "green";
+}
