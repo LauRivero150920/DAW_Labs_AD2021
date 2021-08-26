8aa21a5952507f0e4583d924362cc0496b8c929a
Lau Rivero <A01274144@itesm.mx>
2021-08-26 17:04:19 -0500

    ebento onclick elegante

--- poe.js
+++ poe.js
@@ -7,3 +7,23 @@ function colorea_verde(){
     let elemento = document.getElementById("titulo_menu");
     elemento.style.color = "green";
 }
+
+function muestra_platillos(){
+    const platillos = [];
+    platillos.push("Carne Asada");
+    platillos.push("Spaguetti");
+    platillos.push("Lasagna");
+    platillos.push("Sopa de coditos");
+    platillos.push("Pay de limón");
+
+    let html = "<ul>";
+
+    for(let platillo of platillos){
+        html += "<li>" + platillo + "</li>";
+    }
+
+    html += "</ul>";
+    document.getElementById("platillos").innerHTML = html;
+}
+
+document.getElementById("titulo_menu").onclick = muestra_platillos;
