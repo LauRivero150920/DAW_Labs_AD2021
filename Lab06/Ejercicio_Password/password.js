3d80e11fca76b420be610969bd57e12f83ce8367
Lau Rivero <A01274144@itesm.mx>
2021-08-28 01:35:39 -0500

    Lab06, ejercicio password, checks, falta logica para que lo hagan correctamente

--- Lab06/Ejercicio_Password/password.js
+++ Lab06/Ejercicio_Password/password.js
@@ -1,3 +1,8 @@
+var spec = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;  
+let upperFlag = false;
+let spec_flag;
+let i = 0;
+
 function equal_passwords(){
     let password_1 = document.getElementById("pass1").value;
     let password_2 = document.getElementById("pass2").value;
@@ -6,20 +11,79 @@ function equal_passwords(){
         return true;
     }
     else{
+        document.getElementById("pass_eq").style.color = "red";
         return false;
     }
 }
 
-document.getElementById("pass_submit").onclick = equal_passwords;
-
-function character_length(){
-
-}
-
-function password_strength(){
-    if(special_character() && capital_letter() && character_length()){
-        alert("strong password");
+function character_length(password_1){
+    if(password_1.length >= 8){
+        return true;
+    }
+    else{
+        return false;
     }
 }
 
+function special_character(password_1){
+    if(spec.test(password_1)){
+        spec_flag = true;
+        return true;
+    }
+    spec_flag = false;
+    return false;
+}
 
+function upper_case(password_1){
+    i = password_1.length;
+    if(password_1.charAt(i-1) === password_1.charAt(i-1).toUpperCase() && isNaN(password_1.charAt(i-1)) === true && spec_flag === false){
+        return true;
+    }
+    return false;
+}
+
+function has_number(password_1){
+    let h = password_1.length
+    if(isNaN(password_1.charAt(h-1)) === false && password_1 != ""){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+document.getElementById("pass2").onkeyup = equal_passwords;
+document.getElementById("pass1").onkeyup = password_strength;
+//document.getElementById("pass1").onkeyup = upper_case;
+//document.getElementById("pass1").onkeyup = special_character;
+
+function password_strength(){
+    let password_1 = document.getElementById("pass1").value;
+    if(character_length(password_1)){
+        document.getElementById("char_length").style.color = "green";
+    }
+    else{
+        document.getElementById("char_length").style.color = "red";
+    }
+
+    if(upper_case(password_1)){
+        document.getElementById("upper_case").style.color = "green";
+    }
+    else{
+        document.getElementById("upper_case").style.color = "red";
+    }
+
+    if(special_character(password_1)){
+        document.getElementById("spec_char").style.color = "green";
+    }
+    else{
+        document.getElementById("spec_char").style.color = "red";
+    }
+
+    if(has_number(password_1)){
+        document.getElementById("num").style.color = "green";
+    }
+    else{
+        document.getElementById("num").style.color = "red";
+    }
+}
