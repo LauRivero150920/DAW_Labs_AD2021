0191191c498b8e819de3d7ae07ff8b7d3a4f90bf
Lau Rivero <A01274144@itesm.mx>
2021-08-28 19:41:35 -0500

    Lab06: ejecicio password, terminado

--- Lab06/Ejercicio_Password/password.js
+++ Lab06/Ejercicio_Password/password.js
@@ -1,18 +1,30 @@
-var spec = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;  
+var spec = "/^[!@#$%^&*()_+\-=\[\]{};':\\|,.<>\/?]*$/";  
 let upperFlag = false;
 let spec_flag;
-let i = 0;
 
-function equal_passwords(){
-    let password_1 = document.getElementById("pass1").value;
-    let password_2 = document.getElementById("pass2").value;
-    if(password_1 === password_2){
-        document.getElementById("pass_eq").style.color = "green";
-        return true;
-    }
+function show_password1() {
+    let x = document.getElementById("pass1");
+    if (x.type === "password") {
+        x.type = "text";
+    } 
     else{
-        document.getElementById("pass_eq").style.color = "red";
-        return false;
+        x.type = "password";
+    }
+}
+
+function show_password2() {
+    let x = document.getElementById("pass2");
+    if (x.type === "password") {
+        x.type = "text";
+    } 
+    else{
+        x.type = "password";
+    }
+}
+
+function equal_passwords(password_1, password_2){
+    if(password_1 === password_2 && password_1 != ""){
+        return true;
     }
 }
 
@@ -26,57 +38,132 @@ function character_length(password_1){
 }
 
 function special_character(password_1){
-    if(spec.test(password_1) && password_1 != ""){
-        spec_flag = true;
-        return true;
+    for(let i = 0; i < password_1.length; i++){
+        for(let j = 0; j < spec.length; j++){
+            if(password_1.charAt(i) === spec.charAt(j)){
+                spec_flag = true;
+                return true, spec_flag;
+            }
+        }
     }
     spec_flag = false;
+    return spec_flag;
 }
 
-function upper_case(password_1){
-    i = password_1.length;
-    if(password_1.charAt(i-1) === password_1.charAt(i-1).toUpperCase() && isNaN(password_1.charAt(i-1)) === true && spec_flag === false){
-        return true;
+function upper_case(password_1){ //!A
+    for(let i = 0; i < password_1.length; i++){
+        if(password_1.charAt(i) === password_1.charAt(i).toUpperCase() && isNaN(password_1.charAt(i))){
+            if(spec_flag == true){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
     }
 }
 
 function has_number(password_1){
-    let h = password_1.length
-    if(isNaN(password_1.charAt(h-1)) === false && password_1 != ""){
-        return true;
+    for(let i = 0; i < password_1.length; i++){
+        if(isNaN(password_1.charAt(i)) === false){
+            return true;
+        }
     }
 }
 
-document.getElementById("pass2").onkeyup = equal_passwords;
-document.getElementById("pass1").onkeyup = password_strength;
+document.getElementById("pass_submit").onclick = password_strength;
 
 function password_strength(){
+    let total_stength = 0;
     let password_1 = document.getElementById("pass1").value;
+    let password_2 = document.getElementById("pass2").value;
     if(character_length(password_1)){
         document.getElementById("char_length").style.color = "green";
+        total_stength++;
     }
     else{
         document.getElementById("char_length").style.color = "red";
+        if(total_stength > 0){
+            total_stength--;
+        }
+        else{
+            total_stength = 0;
+        }
     }
 
     if(upper_case(password_1)){
         document.getElementById("upper_case").style.color = "green";
+        total_stength++;
     }
     else{
         document.getElementById("upper_case").style.color = "red";
-    }
-
-    if(special_character(password_1)){
-        document.getElementById("spec_char").style.color = "green";
-    }
-    else{
-        document.getElementById("spec_char").style.color = "red";
+        if(total_stength > 0){
+            total_stength--;
+        }
+        else{
+            total_stength = 0;
+        }
     }
 
     if(has_number(password_1)){
         document.getElementById("num").style.color = "green";
+        total_stength++;
     }
     else{
         document.getElementById("num").style.color = "red";
+        if(total_stength > 0){
+            total_stength--;
+        }
+        else{
+            total_stength = 0;
+        }
+    }
+
+    if(special_character(password_1)){
+        document.getElementById("spec_char").style.color = "green";
+        total_stength++;
+    }
+    else{
+        document.getElementById("spec_char").style.color = "red";
+        if(total_stength > 0){
+            total_stength--;
+        }
+        else{
+            total_stength = 0;
+        }
+    }
+
+    if(equal_passwords(password_1, password_2)){
+        document.getElementById("pass_eq").style.color = "green";
+    }
+    else{
+        document.getElementById("pass_eq").style.color = "red";
+    }
+
+    console.log(total_stength);
+    if(total_stength === 0){
+        document.getElementById("strength_bar").innerHTML = "Low";
+        document.getElementById("strength_bar").style.width = "7%";
+        document.getElementById("strength_bar").style.backgroundColor = "red";
+        document.getElementById("strength_bar").style.textAlign = "left";
+        document.getElementById("strength_bar").style.color = "white";
+        document.getElementById("strength_bar").style.marginLeft = "39.5%";
+    }
+    else if(total_stength > 0 && total_stength < 3){
+        document.getElementById("strength_bar").innerHTML = "Medium";
+        document.getElementById("strength_bar").style.width = "14%";
+        document.getElementById("strength_bar").style.backgroundColor = "yellow";
+        document.getElementById("strength_bar").style.textAlign = "left";
+        document.getElementById("strength_bar").style.color = "black";
+        document.getElementById("strength_bar").style.marginLeft = "40%";
+    }
+    else if(total_stength > 3){
+        document.getElementById("strength_bar").innerHTML = "Secure!";
+        document.getElementById("strength_bar").style.width = "20%";
+        document.getElementById("strength_bar").style.backgroundColor = "green";
+        document.getElementById("strength_bar").style.textAlign = "left";
+        document.getElementById("strength_bar").style.color = "white";
+        document.getElementById("strength_bar").style.marginLeft = "40%";
+        
     }
 }
