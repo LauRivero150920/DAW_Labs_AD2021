a606d76a29634029aab649bad418d9e099c97fd5
Lau Rivero <A01274144@itesm.mx>
2021-09-02 17:50:23 -0500

    Lab09: Ejercicio 3

--- lab09.js
+++ lab09.js
@@ -13,3 +13,25 @@ let write = "Hola mundo";
 const file_system = require('fs');
 
 file_system.writeFileSync("ejercicio2.txt", write);
+
+// Ejercicio 3
+let order = [12,12,45,524,1213,23,2,22,500,912];
+let mayor = 0;
+let menor = 0;
+for(let i = 0; i < order.length; i++){
+    if(i === 0){
+        mayor = order[i];
+        menor = order[i];
+    }
+    else{
+        if(order[i] > mayor){
+            mayor = order[i];
+        }
+        else if(order[i] < menor){
+            menor = order[i];
+        }
+    }
+}
+
+console.log("El número mayor es: ", mayor);
+console.log("El número menor es: ", menor);
