0fbd193926e6afe29de1ebea872d7034a7cfcb75
Lau Rivero <A01274144@itesm.mx>
2021-09-07 23:38:58 -0500

    Lab10: formularios funcionando, recibe y despliega datos del usuario

--- Lab10/routes.js
+++ Lab10/routes.js
@@ -1,3 +1,12 @@
+let animalitos = [
+    {nombre: "Panda Rojo", pais: "Nepal"},
+    {nombre: "Panda Gigante", pais: "China"},
+    {nombre: "Koala" , pais: "Australia"},
+    {nombre: "Perezoso", pais: "Nicaragua"},
+    {nombre: "Oso Grizzly", pais: "Canadá"},
+    {nombre: "Oso Polar", pais: "Groenlandia"}
+];
+
 const requestHandler = (request, response) => {
     console.log(request.url);
 
@@ -58,8 +67,67 @@ const requestHandler = (request, response) => {
         response.write('<head><meta charset="UTF-8"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></head>');
         response.write('<br>');
         response.write('<h1>Aqui recolectamos datos</h1>');
-        response.write('</body>')
+        response.write('<ul style="list-style-type:none;">');
+        for(let animalito of animalitos){
+            response.write('<li style="font-size: 30px">' + animalito.nombre + ': ' + animalito.pais + '</li>');
+        }
+        response.write("</ul>");
+        response.write('<a href = "/datoooss/agregar" style="text-align:center; font-size: 25px">Agregar Animalito </a>');
+        response.write('<br><br><br>');
+        response.write('<a href = "/" style="font-size: 25px; text-align:center">Volver al selector de rutas ʕ•ᴥ•ʔ</a>');
+        response.write('</body>');
         response.end();
+    }
+
+    else if(request.url === "/datoooss/agregar" && request.method === "GET"){
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<body style="background-color: #aee7f5; color: black; text-align:center">');
+        response.write('<head><meta charset="UTF-8"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></head>');
+        response.write('<br>');
+        response.write('<h1 style="text-align:center">Introducir Nuevo Animalito ʕᵔᴥᵔʔ</h1>');
+        response.write('<br>');
+        response.write('<form action="/datoooss/agregar" method="POST">');
+        response.write('<label for="nombre">Nombre de Animalito:  </label>');
+        response.write('<input type="text" id="nombre" name="nombre" placeholder="Panda" required style="margin-left:10px">');
+        response.write('<br><br>');
+        response.write('<label for="pais">País donde se puede encontrar el animalito:  </label>');
+        response.write('<input type="text" id="pais" name="pais" placeholder="China" style="margin-left:10px">');
+        response.write('<br><br>');
+        response.write('<input type="submit" id="enviar" name="enviar" value="Enviar">');
+        response.write('</form>');
+        response.write('<br>');
+        response.write('<a href = "/" style="font-size: 25px">Volver al selector de rutas ʕ•ᴥ•ʔ</a>');
+        response.write('</body>');
+    }
+
+    else if(request.url === "/datoooss/agregar" && request.method === "POST"){
+        console.log(request.method);
+        // Recibir datos del cliente
+        const datos = [];
+        request.on('data', (dato) =>{
+            console.log(dato);
+            datos.push(dato);
+        });
+        console.log(datos);
+
+        // Procesar datos del cliente
+        return request.on('end', () =>{
+            const datos_completos = Buffer.concat(datos).toString();
+            console.log(datos);
+            console.log(datos_completos);
+            const nombre = datos_completos.split('=')[1].split('&')[0];
+            const pais = datos_completos.split('=')[2].split('&')[0];
+            console.log(nombre);
+            console.log(pais);
+
+            // Agregar animalitos
+            animalitos.push({nombre: nombre, pais:pais});
+
+            // Redirección Hacia menú
+            response.statusCode = 302;
+            response.setHeader('Location', '/datoooss');
+            response.end();
+        });
     }
 
     else{
