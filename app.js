fec06acf049e9535a3fd65946aac556e4580fd87
Lau Rivero <A01274144@itesm.mx>
2021-09-20 17:20:32 -0500

    Configurar rutas para get y post

--- app.js
+++ app.js
@@ -31,9 +31,32 @@ app.use('/ruta', (request, response, next) => {
     response.send('Respuesta de la ruta "/ruta"'); 
 });
 
+app.get('/menu/add', (request, response, next) => {
+    let respuesta = '<head><meta charset="UTF-8"></head>';
+    respuesta += '<h1>Agregar platillo al menú</h1>';
+    respuesta += '<form action="/menu/add" method="POST">';
+    respuesta += '<label for="nombre">Nombre de platillo: </label>';
+    respuesta += '<input type="text" id="nombre" name="nombre" placeholder="tacos" required>';
+    respuesta += '<br/>';
+    respuesta += '<br/>';
+    respuesta += '<label for="descripcion">Descripción del platillo: </label>';
+    respuesta += '<input type="text" id="descripcion" name="descripcion" placeholder="Este platillo es delicioso, lleva...">';
+    respuesta += '<br/>';
+    respuesta += '<br/>';
+    respuesta += '<input type="submit" id="enviar" name="enviar" value="Enviar">';
+    respuesta += '</form>';
+    response.send(respuesta);
+});
+
+app.post('/menu/add', (request, response, next) => {
+    response.send("Gracias por contribuir con el menú");
+});
+
 app.use((request, response, next) => {
     console.log('Segundo middleware!');
     response.send('Error 404'); //Manda la respuesta
 });
+
+
 
 app.listen(3000);
