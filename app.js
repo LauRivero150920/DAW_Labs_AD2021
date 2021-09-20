d638a0e1e6c7c312889bf65f73c5b2dce7075067
Lau Rivero <A01274144@itesm.mx>
2021-09-20 17:51:23 -0500

    Creacion de rutas

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+const rutasMenu = require('./routes/menu');
 
 //Middleware
-
-/*app.use( (request, response,next) => {
-    next(); // Solo avanza al siguiente middleware
-});*/
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/menu', rutasMenu);
 
 app.use((request, response, next) => {
     console.log('Primer Middleware!');
@@ -16,47 +15,14 @@ app.use((request, response, next) => {
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
-app.use('/ruta/nombres', (request, response, next) => {
-    console.log('Segundo middleware! (ruta)');
-    response.send('Respuesta de la ruta "/ruta/nombres"'); 
-});
-
-app.use('/ruta/apellidos', (request, response, next) => {
-    console.log('Segundo middleware! (ruta)');
-    response.send('Respuesta de la ruta "/ruta/apellidos"'); 
-});
-
 app.use('/ruta', (request, response, next) => {
     console.log('Segundo middleware! (ruta)');
     response.send('Respuesta de la ruta "/ruta"'); 
 });
 
-app.get('/menu/add', (request, response, next) => {
-    let respuesta = '<head><meta charset="UTF-8"></head>';
-    respuesta += '<h1>Agregar platillo al menú</h1>';
-    respuesta += '<form action="/menu/add" method="POST">';
-    respuesta += '<label for="nombre">Nombre de platillo: </label>';
-    respuesta += '<input type="text" id="nombre" name="nombre" placeholder="tacos" required>';
-    respuesta += '<br/>';
-    respuesta += '<br/>';
-    respuesta += '<label for="descripcion">Descripción del platillo: </label>';
-    respuesta += '<input type="text" id="descripcion" name="descripcion" placeholder="Este platillo es delicioso, lleva...">';
-    respuesta += '<br/>';
-    respuesta += '<br/>';
-    respuesta += '<input type="submit" id="enviar" name="enviar" value="Enviar">';
-    respuesta += '</form>';
-    response.send(respuesta);
-});
-
-app.post('/menu/add', (request, response, next) => {
-    response.send("Gracias por contribuir con el menú");
-});
-
 app.use((request, response, next) => {
     console.log('Segundo middleware!');
-    response.send('Error 404'); //Manda la respuesta
+    response.status(404).send('Recurso no encontrado');
 });
-
-
 
 app.listen(3000);
---
+++ routes/menu.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add', (request, response, next) => {
+    let respuesta = '<head><meta charset="UTF-8"></head>';
+    respuesta += '<h1>Agregar platillo al menú</h1>';
+    respuesta += '<form action="/menu/add" method="POST">';
+    respuesta += '<label for="nombre">Nombre de platillo: </label>';
+    respuesta += '<input type="text" id="nombre" name="nombre" placeholder="tacos" required>';
+    respuesta += '<br/>';
+    respuesta += '<br/>';
+    respuesta += '<label for="descripcion">Descripción del platillo: </label>';
+    respuesta += '<input type="text" id="descripcion" name="descripcion" placeholder="Este platillo es delicioso, lleva...">';
+    respuesta += '<br/>';
+    respuesta += '<br/>';
+    respuesta += '<input type="submit" id="enviar" name="enviar" value="Enviar">';
+    respuesta += '</form>';
+    response.send(respuesta);
+});
+
+router.post('/add', (request, response, next) => {
+    // console.log(request.body.nombre);
+    response.send("Gracias por contribuir con el menú");
+});
+
+router.get('/ruta', (request, response, next) => {
+    response.send('Respuesta de la ruta "/modulo/ruta"'); 
+});
+
+module.exports = router;
