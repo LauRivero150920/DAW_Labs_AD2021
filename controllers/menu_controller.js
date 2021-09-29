02b3845972b1006286e0cd2cb3cffca0447adf0e
Lau Rivero <32498887+LauRivero150920@users.noreply.github.com>
2021-09-29 13:41:30 -0500

    Separar el controlador de las rutas en el módulo menú

--- controllers/menu_controller.js
+++ controllers/menu_controller.js
@@ -7,9 +7,31 @@ const platillos = [
     {nombre: "barbacoa", descripcion: "barbacoa hidalguense", imagen:"https://cdn1.matadornetwork.com/blogs/2/2020/04/Plato-de-barbacoa-900x506.jpg"}
 ];
 
-exports.list = (request, response, next) => {
+exports.getList = (request, response, next) => {
     response.render('lista_menu',  {
         titulo: "Menu",
         lista_platillos: platillos
     });
+};
+
+exports.getAdd = (request, response, next) => {
+    let respuesta = '<head><meta charset="UTF-8"></head>';
+    respuesta += '<h1>Agregar platillo al menú</h1>';
+    respuesta += '<form action="/menu/add" method="POST">';
+    respuesta += '<label for="nombre">Nombre de platillo: </label>';
+    respuesta += '<input type="text" id="nombre" name="nombre" placeholder="tacos" required>';
+    respuesta += '<br/>';
+    respuesta += '<br/>';
+    respuesta += '<label for="descripcion">Descripción del platillo: </label>';
+    respuesta += '<input type="text" id="descripcion" name="descripcion" placeholder="Este platillo es delicioso, lleva...">';
+    respuesta += '<br/>';
+    respuesta += '<br/>';
+    respuesta += '<input type="submit" id="enviar" name="enviar" value="Enviar">';
+    respuesta += '</form>';
+    response.send(respuesta);
+};
+
+exports.postAdd = (request, response, next) => {
+    // console.log(request.body.nombre);
+    response.send("Gracias por contribuir con el menú");
 };
--- routes/menu.js
+++ routes/menu.js
@@ -3,32 +3,10 @@ const express = require('express');
 const menuController = require('../controllers/menu_controller');
 
 const router = express.Router();
-router.get('/list', menuController.list);
+router.get('/list', menuController.getList);
 
-router.get('/add', (request, response, next) => {
-    let respuesta = '<head><meta charset="UTF-8"></head>';
-    respuesta += '<h1>Agregar platillo al menú</h1>';
-    respuesta += '<form action="/menu/add" method="POST">';
-    respuesta += '<label for="nombre">Nombre de platillo: </label>';
-    respuesta += '<input type="text" id="nombre" name="nombre" placeholder="tacos" required>';
-    respuesta += '<br/>';
-    respuesta += '<br/>';
-    respuesta += '<label for="descripcion">Descripción del platillo: </label>';
-    respuesta += '<input type="text" id="descripcion" name="descripcion" placeholder="Este platillo es delicioso, lleva...">';
-    respuesta += '<br/>';
-    respuesta += '<br/>';
-    respuesta += '<input type="submit" id="enviar" name="enviar" value="Enviar">';
-    respuesta += '</form>';
-    response.send(respuesta);
-});
+router.get('/add', menuController.getAdd);
 
-router.post('/add', (request, response, next) => {
-    // console.log(request.body.nombre);
-    response.send("Gracias por contribuir con el menú");
-});
-
-router.get('/ruta', (request, response, next) => {
-    response.send('Respuesta de la ruta "/modulo/ruta"'); 
-});
+router.post('/add', menuController.postAdd);
 
 module.exports = router;
