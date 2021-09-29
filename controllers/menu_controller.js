75c7ac9ae700d9784ebed0e09018a00b80227d21
Lau Rivero <32498887+LauRivero150920@users.noreply.github.com>
2021-09-29 14:25:30 -0500

    Refactorizar código existente

--- controllers/menu_controller.js
+++ controllers/menu_controller.js
@@ -8,25 +8,15 @@ exports.getList = (request, response, next) => {
 };
 
 exports.getAdd = (request, response, next) => {
-    let respuesta = '<head><meta charset="UTF-8"></head>';
-    respuesta += '<h1>Agregar platillo al menú</h1>';
-    respuesta += '<form action="/menu/add" method="POST">';
-    respuesta += '<label for="nombre">Nombre de platillo: </label>';
-    respuesta += '<input type="text" id="nombre" name="nombre" placeholder="tacos" required>';
-    respuesta += '<br/>';
-    respuesta += '<br/>';
-    respuesta += '<label for="descripcion">Descripción del platillo: </label>';
-    respuesta += '<input type="text" id="descripcion" name="descripcion" placeholder="Este platillo es delicioso, lleva...">';
-    respuesta += '<br/>';
-    respuesta += '<br/>';
-    respuesta += '<input type="submit" id="enviar" name="enviar" value="Enviar">';
-    respuesta += '</form>';
-    response.send(respuesta);
+    
+    response.render('add_menu',{
+        titulo: "Agregar Platillo al Menú",
+    });
 };
 
 exports.postAdd = (request, response, next) => {
     // console.log(request.body.nombre);
     const platillo = new Platillo(request.body.nombre, request.body.descripcion, "https://dam.cocinafacil.com.mx/wp-content/uploads/2020/04/comida-china-tipica.jpg");
     platillo.save();
-    response.send("Gracias por contribuir con el menú");
+    response.status(302).redirect('/menu/list');
 };
