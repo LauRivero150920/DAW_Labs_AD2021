507e8f239b21effa4d4ba9f8f8edec7e17dab74e
Lau Rivero <32498887+LauRivero150920@users.noreply.github.com>
2021-09-30 17:01:51 -0500

    Generar y recuperar cookies

--- controllers/menu_controller.js
+++ controllers/menu_controller.js
@@ -1,6 +1,8 @@
 const Platillo = require('../models/platillo');
 
 exports.getList = (request, response, next) => {
+    console.log(request.get('Cookie'));
+    console.log(request.get('Cookie').split(';')[0].trim().split('=')[1]);
     response.render('lista_menu',  {
         titulo: "Menu",
         lista_platillos: Platillo.fetchAll()
@@ -14,7 +16,7 @@ exports.getAdd = (request, response, next) => {
 };
 
 exports.postAdd = (request, response, next) => {
-    // console.log(request.body.nombre);
+    response.setHeader('Set-Cookie', 'ultimo_platillo='+request.body.nombre);
     const platillo = new Platillo(request.body.nombre, request.body.descripcion, "https://dam.cocinafacil.com.mx/wp-content/uploads/2020/04/comida-china-tipica.jpg");
     platillo.save();
     response.status(302).redirect('/menu/list');
