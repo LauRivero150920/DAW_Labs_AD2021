46dbf37c0a6e01e2b1eb3c3232d22a4111f39628
LauRivero150920 <A01274144@itesm.mx>
2021-10-08 21:36:40 -0500

    crear modelo de usuario

--- models/usuario.js
+++ models/usuario.js
@@ -2,21 +2,21 @@ const db = require('../util/database');
 const bcrypt = require('bcryptjs');
 
 module.exports = class Usuario {
-    //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(nombre_completo, username, password) {
-        this.nombre = nombre_completo;
+    //* Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
+    constructor(nombre, username, password) {
+        this.nombre = nombre;
         this.username = username;
         this.password = password;
     }
 
-    //Este método servirá para guardar de manera persistente el nuevo objeto. 
+    //* Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-        return db.execute('INSERT INTO usuarios (nombre_completo, username, password) VALUES (?, ?, ?)',
-            [this.nombre_completo, this.username, bcrypt.hashSync(this.password, 12)]
+        return db.execute('INSERT INTO usuarios (nombre, username, password) VALUES (?, ?, ?)',
+            [this.nombre, this.username, bcrypt.hashSync(this.password, 12)]
         );
     }
 
-    //Este método servirá para devolver los objetos del almacenamiento persistente.
+    //* Este método servirá para devolver los objetos del almacenamiento persistente.
     static fetchOne(username, password) {
         return db.execute('SELECT * FROM usuarios WHERE username = ? AND password = ?',
             [username, password]);
