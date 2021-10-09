9f4157083400681a92b61b0f91603c05e1d06de2
LauRivero150920 <A01274144@itesm.mx>
2021-10-08 19:40:23 -0500

    Conexión con la base de datos 2

--- util/database.js
+++ util/database.js
@@ -1,3 +1,4 @@
+//? Intentar hacerlo con posgres (PGAdmin)
 const mysql = require('mysql2');
 
 const BD = "DEV";
@@ -7,12 +8,18 @@ let user = '';
 let database = '';
 let password = '';
 
+
+//? Variables de entorno
+
+//* Fase de development (desarrollo)
 if(BD ===  "DEV"){
     host = 'localhost';
     user = 'root';
     database = 'comida';
     password = '';
 }
+
+//* Fase de producción, no debe estar versionada
 else if(BD === "PROD"){
     host = 'localhost';
     user = 'root';
@@ -20,11 +27,14 @@ else if(BD === "PROD"){
     password = 'fewfednvjerwf243dkmsjvf4_#_#nrehufiwodjskc12342jnfwejdioskmncdj';
 }
 
+//? Intentar hacerlo con posgres (PGAdmin)
 const pool = mysql.createPool({
     host: host,
     user: user,
     database: database,
-    password: password
+    password: password,
 });
 
+//* Una función que ejecuta otra función y se queda esperando a que se cumpla lo prometido
+//* El código cumple su función normal hasta que se cumpla o no la promesa
 module.exports = pool.promise();
